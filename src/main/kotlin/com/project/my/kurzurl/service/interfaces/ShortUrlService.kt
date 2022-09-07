ea176797e9d825107658895d3d85b63b755e55a7
Mohsen Mirzaei <mohsen.mirzaei@dfreight.org>
2022-09-07 10:44:33 +0430

    added create short URL service.

---
+++ src/main/kotlin/com/project/my/kurzurl/service/interfaces/ShortUrlService.kt
@@ -0,0 +1,7 @@
+package com.project.my.kurzurl.service.interfaces
+
+import com.project.my.kurzurl.`in`.CreateShortUrlInDto
+
+interface ShortUrlService {
+    fun createShortUrl(dto: CreateShortUrlInDto?): String?
+}
