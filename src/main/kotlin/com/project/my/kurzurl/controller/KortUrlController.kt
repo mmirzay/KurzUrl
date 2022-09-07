b4febeac084e44f78e66604d5907850a932f076e
Mohsen Mirzaei <mohsen.mirzaei@dfreight.org>
2022-09-07 12:46:26 +0430

    revised controllers APIs based on Action result.

--- src/main/kotlin/com/project/my/kurzurl/controller/KortUrlController.kt
+++ src/main/kotlin/com/project/my/kurzurl/controller/KortUrlController.kt
@@ -1,19 +1,30 @@
 package com.project.my.kurzurl.controller
 
 import com.project.my.kurzurl.`in`.CreateShortUrlInDto
-import lombok.RequiredArgsConstructor
+import com.project.my.kurzurl.out.ActionResult
 import org.springframework.web.bind.annotation.*
+import java.lang.Boolean
+import kotlin.String
 
 @RestController
-@RequiredArgsConstructor
 class KortUrlController {
+
+
     @PostMapping("/create")
-    fun createShortUrl(@RequestBody dto: CreateShortUrlInDto?): String {
-        return "shortUrl"
+    fun createShortUrl(@RequestBody dto: CreateShortUrlInDto?): ActionResult<String> {
+        return ActionResult.Builder<String>()
+            .success(Boolean.TRUE)
+            .data("shortURL")
+            .message("ShortURL created Successfully.")
+            .build()
     }
 
     @GetMapping("/{shortUrl}")
-    fun createShortUrl(@PathVariable shortUrl: String?): String {
-        return "LongURL"
+    fun createShortUrl(@PathVariable shortUrl: String?): ActionResult<String> {
+        return ActionResult.Builder<String>()
+            .success(Boolean.TRUE)
+            .data("LongURL")
+            .message("LongURL is retrieved Successfully.")
+            .build()
     }
 }
