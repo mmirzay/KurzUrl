1a377e7ad88d1a86d4cda2b3f3dc0fef748c7ad0
Mohsen Mirzaei <mohsen.mirzaei@dfreight.org>
2022-09-08 17:49:51 +0430

    revised create short url services to handle duplicate results.

--- src/main/kotlin/com/project/my/kurzurl/repository/UrlRepository.kt
+++ src/main/kotlin/com/project/my/kurzurl/repository/UrlRepository.kt
@@ -2,6 +2,10 @@ package com.project.my.kurzurl.repository
 
 import com.project.my.kurzurl.entity.Url
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface UrlRepository : BaseJpaRepository<Url, String>
+interface UrlRepository : BaseJpaRepository<Url, String> {
+
+    fun findByShortUrl(shortUrl: String): Optional<Url>
+}
--- src/main/kotlin/com/project/my/kurzurl/service/impl/ShortUrlServiceImpl.kt
+++ src/main/kotlin/com/project/my/kurzurl/service/impl/ShortUrlServiceImpl.kt
@@ -6,8 +6,8 @@ import com.project.my.kurzurl.repository.UrlRepository
 import com.project.my.kurzurl.service.interfaces.ShortUrlService
 import com.project.my.kurzurl.utility.UrlIdentifierUtil
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class ShortUrlServiceImpl(val urlRepository: UrlRepository) : ShortUrlService {
@@ -18,10 +18,21 @@ class ShortUrlServiceImpl(val urlRepository: UrlRepository) : ShortUrlService {
     override fun createShortUrl(dto: CreateShortUrlInDto): String {
         logger.info("creating new short url")
         val shortURL = UrlIdentifierUtil.identifierOf(dto.url)
+        if(shortUrlExists(shortURL))
+            return shortURL;
+
         val url: Url = dto.toUrl(shortURL)
         var save: Url = urlRepository!!.save(url)
         logger.info("New short URL with id [{}] created successfully", save.id)
         return shortURL
+    }
+
+    private fun shortUrlExists(shortURL: String): Boolean {
+        return findUrl(shortURL).isPresent
+    }
+
+    fun findUrl(shortUrl: String): Optional<Url> {
+        return urlRepository.findByShortUrl(shortUrl)
     }
 
     override fun getLongUrl(shortUrl: String?): String? {
