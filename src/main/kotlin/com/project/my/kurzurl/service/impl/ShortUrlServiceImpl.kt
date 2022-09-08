e6e524b759248d532d0c00360ce56dbacfd2f21b
Mohsen Mirzaei <mohsen.mirzaei@dfreight.org>
2022-09-08 16:15:11 +0430

    added implementation for saving dummy URL

--- src/main/kotlin/com/project/my/kurzurl/service/impl/ShortUrlServiceImpl.kt
+++ src/main/kotlin/com/project/my/kurzurl/service/impl/ShortUrlServiceImpl.kt
@@ -1,15 +1,28 @@
 package com.project.my.kurzurl.service.impl
 
 import com.project.my.kurzurl.`in`.CreateShortUrlInDto
+import com.project.my.kurzurl.entity.Url
+import com.project.my.kurzurl.repository.UrlRepository
 import com.project.my.kurzurl.service.interfaces.ShortUrlService
-import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
-@Slf4j
 @Service
 class ShortUrlServiceImpl : ShortUrlService {
-    override fun createShortUrl(dto: CreateShortUrlInDto?): String? {
-        return "shortUrl"
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @Autowired
+    private val urlRepository: UrlRepository? = null
+
+
+    override fun createShortUrl(dto: CreateShortUrlInDto?): String {
+        logger.info("creating new short url")
+        val url: Url = dto!!.toUrl("shortUrl")
+        var save: Url = urlRepository!!.save(url)
+        logger.info("New short URL with id [{}] created successfully", save.id)
+        return "shortUrl"+ save.id.toString()
     }
 
     override fun getLongUrl(shortUrl: String?): String? {
--- src/main/kotlin/com/project/my/kurzurl/service/interfaces/ShortUrlService.kt
+++ src/main/kotlin/com/project/my/kurzurl/service/interfaces/ShortUrlService.kt
@@ -3,6 +3,6 @@ package com.project.my.kurzurl.service.interfaces
 import com.project.my.kurzurl.`in`.CreateShortUrlInDto
 
 interface ShortUrlService {
-    fun createShortUrl(dto: CreateShortUrlInDto?): String?
+    fun createShortUrl(dto: CreateShortUrlInDto?): String
     fun getLongUrl(shortUrl: String?): String?
 }
